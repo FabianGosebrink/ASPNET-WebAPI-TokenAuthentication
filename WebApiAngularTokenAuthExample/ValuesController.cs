92426035236c82ebf602c32254b52f90f17adb75
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2015-09-16 18:10:37 +0200

    small changes to WebAPI

--- WebApiAngularTokenAuthExample/ValuesController.cs
+++ WebApiAngularTokenAuthExample/ValuesController.cs
@@ -11,30 +11,9 @@ namespace WebApiAngularTokenAuthExample
     public class ValuesController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            return Ok(new string[] { "value1", "value2" });
         }
     }
 }
